2b1a83d5eac9d606e32ad90d9851d4e874722aa6
jantm <janmakulec@gmail.com>
2019-05-11 18:23:09 +0200

    Initial basic styles

---
+++ src/style/color.js
@@ -0,0 +1,14 @@
+const color = {
+  // max points:
+  black: '#000',
+  white: '#fff',
+
+  // grays:
+  alabaster:     '#fcfcfc',
+  alto:          '#dcdcdc',
+  silverChalice: '#aaa',
+  gray:          '#888',
+  doveGray:      '#666',
+};
+
+export default color;
---
+++ src/style/typography.js
@@ -0,0 +1,15 @@
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
+export const fontSize = {
+  base: wp('4.3%'),
+  l: wp('4.5%'),
+  xl: wp('4.8%'),
+};
+
+export const fontFamily = {
+  sansSerif: 'Futura',
+  monospace: 'Menlo',
+};
