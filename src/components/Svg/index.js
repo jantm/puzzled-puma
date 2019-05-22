8ae6ac7126542397d861c72a8f131ea1f9e3af94
jantm <janmakulec@gmail.com>
2019-05-22 17:49:51 +0200

    Add common SVG helpers

---
+++ src/components/Svg/index.js
@@ -0,0 +1,4 @@
+export { Svg } from 'react-native-svg';
+export { G as SvgGroup } from 'react-native-svg';
+export { default as SvgLine } from './SvgLine';
+export { default as SvgCircle } from './SvgCircle';
